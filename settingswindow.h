7682da0f63b91c7c2b4cbc233be49a88bdd9eac3
fannulain <egord201717@gmail.com>
2025-04-29 15:10:20 +0300

    windows
    1

---
+++ settingswindow.h
@@ -0,0 +1,25 @@
+#ifndef SETTINGSWINDOW_H
+#define SETTINGSWINDOW_H
+
+#include <QWidget>
+
+namespace Ui {
+class SettingsWindow;
+}
+
+class SettingsWindow : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit SettingsWindow(QWidget *parent = nullptr);
+    ~SettingsWindow();
+
+private:
+    Ui::SettingsWindow *ui;
+
+private slots:
+    void on_btn_close_clicked();
+};
+
+#endif // SETTINGSWINDOW_H
